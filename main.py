f36f830ddddbef4019b568db00c9f32a22f88864
Eric Margadonna <eric082659@gmail.com>
2023-01-31 01:04:30 -0500

    Version 1.0 baby!

--- main.py
+++ main.py
@@ -31,6 +31,13 @@ BPT_vocab_dict = {
     "z": "change"
 }
 
+def getFontsForOS(title_size, body_size):
+    import os
+    if os.name == "nt":
+        return ImageFont.truetype("arial.ttf", title_size), ImageFont.truetype("arialbd.ttf", body_size)
+    elif os.name == "posix":
+        return ImageFont.truetype("/Library/Fonts/Arial.ttf", title_size), ImageFont.truetype("/Library/Fonts/Arial Bold.ttf", body_size)
+    
 def getVocabWord(m_letter, m_vocab_dict):
     # Wrapper function to get a word from the vocabulary dictionary
     return m_vocab_dict[m_letter]
@@ -81,7 +88,7 @@ def writePoem(m_seed, m_vocab_dict):
     # Return the title and lines as separate variables
     return m_title, m_lines
 
-def generateImage(m_title, m_lines):
+def generateImage(m_title, m_lines, title_size=20, start_size=9):
     # This function generates an image of the poem
     # It takes in the title and lines of the poem and returns
     # an image object
@@ -91,15 +98,16 @@ def generateImage(m_title, m_lines):
     # each word is used, not including the title.
 
     # Set up the image
-    image = Image.new("RGB", (800, 600), (255, 255, 255))
+    image_width = len(max(m_lines))*15
+    image_height = len(m_lines)*20+ 75
+
+    image = Image.new("RGB", (image_width, image_height), (255, 255, 255))
     draw = ImageDraw.Draw(image)
-    titlefont = ImageFont.truetype("arialbd.ttf", 20,)
+
+    titlefont, bodyfont = getFontsForOS(title_size, start_size)
 
     # Draw the title
     draw.text((10, 10), m_title.upper(), (0, 0, 0), font=titlefont)
-
-    # Define the base font to be used for the body
-    bodyfont = ImageFont.truetype("arial.ttf", 9)
 
     # Set up the word count dictionary
     wordcount = {}
@@ -107,8 +115,12 @@ def generateImage(m_title, m_lines):
     # Set up the line count
     linecount = 0
 
-    # Set up the y position
+    # Set up the x and y position
+    x = 10
     y = 40
+
+    #set padding
+    padding = 2
 
     # For each line in the poem
     for line in m_lines:
@@ -121,20 +133,25 @@ def generateImage(m_title, m_lines):
             else:
                 # Otherwise, add the word to the dictionary with a count of 1
                 wordcount[word] = 1
+
+            #Create a variable for the text to be drawn
+            text = word.upper() + " "
+
+            bodyfont = bodyfont.font_variant(size=wordcount[word] + start_size)
+
             # Draw the word
-            draw.text((10, y), word.upper(), (0, 0, 0), font=bodyfont)
-            # Increment the y position by 10
-            y += 10
-        # Increment the line count by 1
-        linecount += 1
-        # Increment the y position by 10
-        y += 10
+            draw.text((x, y), text, (0, 0, 0), font=bodyfont)
+           
+            #Increment the x position by the size of the word
+            x += bodyfont.getsize(text)[0]
+    
+        # Increment the y position by 10 + the size of the largest word
+        y += max(wordcount.values()) + start_size + padding
+        #Reset the x position
+        x = 10
 
-    filename = m_title.replace(" ", "_") + ".png"
+    filename = "photos/" + m_title.replace(" ", "_") + ".png"
     image.save(filename)
-
-    # Return 0 for success
-    return 0
 
 if __name__ == "__main__":
 
@@ -143,18 +160,18 @@ if __name__ == "__main__":
     # Start by setting the vocabulary for your IBM poem
     # This is done by entering a word for each letter of the alphabet
     
-    # vocab_dict = {k: v  for k, v in 
-    #             zip(string.ascii_lowercase, 
-    #             [input(f"{string.ascii_lowercase[i]}: ").lower() 
-    #             for i in range(0,26)])
-    #             }
+    vocab_dict = {k: v  for k, v in 
+                zip(string.ascii_lowercase, 
+                [input(f"{string.ascii_lowercase[i]}: ").lower() 
+                for i in range(0,26)])
+                }
 
     # Then, enter a three letter word or phrase that acts as a 'seed' for the poem
     
     startletters = input("Enter 3 letter word or phrase (i.e. 'IBM'): ").lower()
     
     # Finally, generate the poem
-    title, lines = writePoem(startletters, BPT_vocab_dict)
+    title, lines = writePoem(startletters, vocab_dict)
 
     # And print it out!
     print()
